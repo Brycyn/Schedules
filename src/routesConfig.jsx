21d95c907a105b1671404fab5f0b07a9394c7e5c
Brycyn <bgossett@westmont.edu>
2025-01-25 19:22:28 -0800

    added Home Page

--- src/routesConfig.jsx
+++ src/routesConfig.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Wage from "./pages/Wage";
 import CalendarEvents from "./pages/Calendar";
+import Home from "./pages/Home";
 const AppRoutes = () => (
   <Routes>
-    <Route path="/" element={<CalendarEvents />} />
+    <Route path="/" element={<Home />} />
     <Route path="/wage" element={<Wage />} />
+    <Route path="/calendar" element={<CalendarEvents />} />
   </Routes>
 );
 
