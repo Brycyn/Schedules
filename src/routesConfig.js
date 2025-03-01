1460e8d5228d1c242315a8b8842bb7e19c71fc64
Brycyn <bgossett@westmont.edu>
2025-03-01 00:49:16 -0800

    added about me page

--- src/routesConfig.js
+++ src/routesConfig.js
@@ -2,6 +2,7 @@ import Wage from "./pages/Wage";
 import CalendarEvents from "./pages/Calendar";
 import Home from "./pages/Home";
 import Chat from "./pages/Chat";
+import AboutMe from './pages/Info'
 import { Routes, Route } from "react-router-dom";
 import socketIO from "socket.io-client";
 
@@ -13,6 +14,8 @@ const AppRoutes = () => (
     <Route path="/wage" element={<Wage socket={socket} />} />
     <Route path="/calendar" element={<CalendarEvents socket={socket} />} />
     <Route path="/chat" element={<Chat socket={socket} />} />
+    <Route path="/Info" element={<AboutMe socket={socket} />} />
+
 
   </Routes>
 );
