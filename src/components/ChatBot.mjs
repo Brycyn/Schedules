92b551b8ec90c623fa45a29b22e0bc1849a7fd19
Brycyn <bgossett@westmont.edu>
2025-03-10 19:45:32 -0700

    added open ai Chatbot

--- src/components/ChatBot.mjs
+++ src/components/ChatBot.mjs
@@ -1,24 +1,45 @@
 import OpenAI from "openai";
-import dotenv from 'dotenv';  // Import dotenv
-
-dotenv.config();
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+import React, { useState } from "react";
+import { NavBar } from "./NavBar.jsx";
+import { IoPaperPlaneOutline } from "react-icons/io5";
 
 
+export default function Bot() {
+  const [systemContent, setSystemContent] = useState()
+  const [userContent, setUserContent] = useState()
+  const [aIResponse, setAIResponse] = useState()
 
-});
 
-const completion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
+
+  async function fetchAIResponse() {
+
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY
+    });
+
+
+
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
         { role: "system", content: "You are a helpful assistant." },
         {
-            role: "user",
-            content: "Write a haiku about Naruto.",
+          role: "user",
+          content: userContent,
         },
-    ],
-    store: true,
-});
+      ],
+      store: true,
+    });
+    setAIResponse(completion.choices[0].message.content)
+    console.log(completion.choices[0].message);
+  }
+  return (<><div><NavBar /></div><div>
 
-console.log(completion.choices[0].message);
+    <p>{aIResponse}</p>
+    <div className="bot-bar" style={{ display: "flex", flexDirection: 'row' }}>
+      <textarea style={{ width: '100%' }} type="textarea" onChange={async (e) => await setUserContent(e.target.value)}></textarea>
+      <IoPaperPlaneOutline onClick={() => fetchAIResponse()} />
+    </div>
+  </div></>)
+}
+
